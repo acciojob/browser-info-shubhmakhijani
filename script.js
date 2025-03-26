e164e3939244210ba6fd7d1f128c588a47deba3a
shubhmakhijani <124859260+shubhmakhijani@users.noreply.github.com>
2025-03-26 05:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const searchContainer = document.querySelector(".search");
+const searchInput = document.querySelector(".input");
+const searchButton = document.querySelector(".btn");
+
+searchButton.addEventListener("click", () => {
+    searchContainer.classList.toggle("active");
+    searchInput.focus();
+});
